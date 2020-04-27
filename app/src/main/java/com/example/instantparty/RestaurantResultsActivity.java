2c832a1f6625493fb8f50667a33bfc44d669e4ca
foodlauncher <launcherfood5@gmail.com>
2020-04-28 01:41:29 +0530

    Sort By

--- app/src/main/java/com/example/instantparty/RestaurantResultsActivity.java
+++ app/src/main/java/com/example/instantparty/RestaurantResultsActivity.java
@@ -1,14 +1,23 @@
 package com.example.instantparty;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.google.android.material.radiobutton.MaterialRadioButton;
+
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -26,12 +35,22 @@ public class RestaurantResultsActivity extends AppCompatActivity {
     private String sortByRating = "rating";
     private String sortByCost = "cost";
     RecyclerView recyclerView;
+    List<Restaurant> restaurants1;
+    List<Restaurant> restaurants2;
+
+    MaterialRadioButton radioRating, radioDistance;
+    RadioGroup radioGroup;
+    Call<Search> call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_results);
         Bundle extras = getIntent().getExtras();
+
+        radioGroup = findViewById(R.id.radio_group);
+        radioDistance = findViewById(R.id.radio_distance);
+        radioRating = findViewById(R.id.radio_rating);
 
         assert extras != null;
         latitude = extras.getDouble("latitude");
@@ -48,8 +67,59 @@ public class RestaurantResultsActivity extends AppCompatActivity {
 
         ApiSearchInterface searchApi = retrofit.create(ApiSearchInterface.class);
 
-        Call<Search> call = searchApi.getResultBy(apiKey, latitude, longitude, DEFAULT_COUNT, sortByRealDistance);
+        if(radioRating.isChecked()) {
+            call = searchApi.getResultBy(apiKey, latitude, longitude, DEFAULT_COUNT, sortByRating);
+            callEnqueue(call);
+        } else {
+            call = searchApi.getResultBy(apiKey, latitude, longitude, DEFAULT_COUNT, sortByRealDistance);
+            callEnqueue(call);
+        }
 
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(radioRating.isChecked()) {
+                    call = searchApi.getResultBy(apiKey, latitude, longitude, DEFAULT_COUNT, sortByRating);
+                } else {
+                    call = searchApi.getResultBy(apiKey, latitude, longitude, DEFAULT_COUNT, sortByRealDistance);
+                }
+                callEnqueue(call);
+            }
+        });
+
+        Call<Search> call2 = searchApi.getResultBy(apiKey, latitude, longitude, DEFAULT_COUNT, sortByRating);
+
+
+
+//        call2.enqueue(new Callback<Search>() {
+//            @Override
+//            public void onResponse(Call<Search> call, Response<Search> response) {
+//                if (!response.isSuccessful()) {
+//                    textViewResult.setText("Response code: " + response.code());
+//                    return;
+//                }
+//                Search search = response.body();
+//
+//                restaurants2 = search.getRestaurants();
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Search> call, Throwable t) {
+//                textViewResult.setText(t.getMessage());
+//            }
+//        });
+
+        //sortRestaurants(restaurants1, restaurants2);
+
+    }
+//
+//    private void sortRestaurants(List<Restaurant> restaurants1, List<Restaurant> restaurants2) {
+//        List<Restaurant> finalRestaurants;
+//
+//        for(int i=0; i<)
+//    }
+
+    private void callEnqueue(Call call) {
         call.enqueue(new Callback<Search>() {
             @Override
             public void onResponse(Call<Search> call, Response<Search> response) {
@@ -59,10 +129,9 @@ public class RestaurantResultsActivity extends AppCompatActivity {
                 }
                 Search search = response.body();
 
-                List<Restaurant> restaurants = search.getRestaurants();
+                restaurants1 = search.getRestaurants();
 
-                recyclerView.setAdapter(new Adapter(restaurants, R.layout.adapter, getApplicationContext()));
-
+                recyclerView.setAdapter(new Adapter(restaurants1, R.layout.adapter, RestaurantResultsActivity.this));
             }
 
             @Override
@@ -70,6 +139,5 @@ public class RestaurantResultsActivity extends AppCompatActivity {
                 textViewResult.setText(t.getMessage());
             }
         });
-
     }
 }
